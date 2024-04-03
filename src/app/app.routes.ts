5f2ec4d94a0eb528f5cb03a5fdf3456f08bbd44e
RiadMefti <riadmefti@hotmail.com>
2024-04-03 19:36:29 -0400

    adding first ebauche of bar-chart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RadialBarChartComponent } from './components/radial-bar-chart/radial-bar-chart.component';
 
 export const routes: Routes = [
     { path: '', title: 'Home Page', component: HomePageComponent},
-    { path: 'home', title: 'Home Page', component: HomePageComponent}
+    { path: 'home', title: 'Home Page', component: HomePageComponent},
+    { path : 'radial', title: 'Radial', component: RadialBarChartComponent},
 ];
