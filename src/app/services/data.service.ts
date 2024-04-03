d25ef4311c28981967a1ce216d8074177a0b1cbb
RiadMefti <riadmefti@hotmail.com>
2024-04-03 12:51:53 -0400

    add dataPreparation for RadialBarChart

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { csv, autoType, DSVParsedArray } from 'd3'
+import { csv, autoType, DSVParsedArray, DSVRowString } from 'd3'
+import { RadialChartData } from '../components/radial-bar-chart/radial-bar-chart.component';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,29 @@ export class DataService {
     const csvFile = '../../assets/actes-criminels.csv';
     return await csv(csvFile, autoType)
   }
+  async prepareDataForRadialBarChart(): Promise<RadialChartData[]> {
+
+    const data: any = await this.getMontrealCrimeData();
+    const count = {
+      'Jours': 0,
+      'Soir': 0,
+      'Nuit': 0
+    };
+
+    data.forEach((row: { [x: string]: any; }) => {
+      // Assuming the QUART column contains the time of day information
+      const timeOfDay = row['QUART'];
+      if (timeOfDay === 'jour') {
+        count['Jours']++;
+      } else if (timeOfDay === 'soir') {
+        count['Soir']++;
+      } else if (timeOfDay === 'nuit') {
+        count['Nuit']++;
+      }
+    });
+
+    return Object.entries(count).map(([name, value]) => ({ name, value }));
+  }
 
   prepareDataStackedAreaChart(data: any) {
     const dataByWeek = data.reduce((accumulator: { [x: string]: { [x: string]: any; }; }, currentValue: { DATE: string | number | Date; CATEGORIE: any; }) => {
@@ -24,14 +48,14 @@ export class DataService {
       const weekNumber = this.getWeek(date);
       const year = date.getFullYear();
       const weekKey = `${year}-S${String(weekNumber).padStart(2, '0')}`;
-    
+
       if (!accumulator[weekKey]) {
         accumulator[weekKey] = {};
       }
-    
+
       const correctedCategory = this.replaceIncorrectCharacters(currentValue.CATEGORIE);
       accumulator[weekKey][correctedCategory] = (accumulator[weekKey][correctedCategory] || 0) + 1;
-    
+
       return accumulator;
     }, {});
 
@@ -40,7 +64,7 @@ export class DataService {
         week,
         ...dataByWeek[week]
       };
-    });    
+    });
 
     preparedData.sort((a, b) => {
       return a.week.localeCompare(b.week);
@@ -61,5 +85,5 @@ export class DataService {
       .replace(/Vols qualifi�s/g, 'Vols qualifiés')
       .replace(/M�fait/g, 'Méfait')
       .replace(/Vol dans \/ sur v�hicule � moteur/g, 'Vol dans / sur véhicule à moteur');
-    }
+  }
 }
