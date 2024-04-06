6765f66dde207010be9a0f6abf99387fd9996bf9
Félix Lamoureux <71103018+Blinkod@users.noreply.github.com>
2024-04-05 20:55:47 -0400

    refactor code for stacked Area chart

--- src/app/components/stacked-area-chart/stacked-area-chart.component.ts
+++ src/app/components/stacked-area-chart/stacked-area-chart.component.ts
@@ -25,45 +25,61 @@ export class StackedAreaChartComponent implements OnInit {
   private svg: any;
   private width: number = 960 - this.margin.left - this.margin.right;
   private height: number = 500 - this.margin.top - this.margin.bottom;
+
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
-    this.getMontrealCrimeData();
+    this.loadGraph();
   }
 
-  private async getMontrealCrimeData() {
-    try {
-      const data = await this.dataService.prepareDataStackedAreaChart();
-      this.drawChart(data);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
+  private async loadGraph() {
+    const data = await this.getMontrealCrimeData();
+    this.setupChart();
+    this.drawChart(data);
   }
 
-  private parseYearMonthToDate(monthString: string): Date {
-    const [year, month] = monthString.split('-').map(Number);
-    return new Date(year, month - 1);
+  private async getMontrealCrimeData(): Promise<any[]> {
+    return await this.dataService.prepareDataStackedAreaChart();
   }
 
-  private drawChart(data: any) {
+
+  private setupChart() {
     const element = this.chartContainer.nativeElement;
-  
-    const categories = Object.keys(data[0]).filter(k => k !== 'month');
     this.svg = d3.select(element)
       .append('svg')
       .attr('width', this.width + this.margin.left + this.margin.right)
       .attr('height', this.height + this.margin.top + this.margin.bottom)
       .append('g')
       .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
+  }
 
+  private drawChart(data: any) {
+    const categories = this.getCategories(data);
+    const dates = this.getDates(data);
+    const x = this.createXScale(dates);
+    const y = this.createYScale(data, categories);
 
-    const dates: Date[] = data
-      .map((d: any) => this.parseYearMonthToDate(d.month));
-      
-    const x = d3.scaleTime()
+    this.drawAxes(x, y);
+    this.drawStackedAreas(data, categories, x, y);
+    this.drawLegend(categories);
+    this.annotateSpecialDates(x);
+  }
+
+  private getCategories(data: any[]): string[] {
+    return Object.keys(data[0]).filter(k => k !== 'month');
+  }
+
+  private getDates(data: any[]): Date[] {
+    return data.map((d: any) => this.parseYearMonthToDate(d.month));
+  }
+
+  private createXScale(dates: Date[]) {
+    return d3.scaleTime()
       .domain(d3.extent(dates) as [Date, Date])
       .range([0, this.width]);
+  }
 
+  private createYScale(data: any[], categories: string[]) {
     let yMax = d3.max(data, (d: DataPoint) => 
       d3.max(categories, cat => {
         let value = 0;
@@ -74,36 +90,12 @@ export class StackedAreaChartComponent implements OnInit {
         return value;
       })
     );
-
-    if(typeof yMax === 'number') {
-      yMax += 1000;
-    }
-
-    const y = d3.scaleLinear()
-      .domain([0, yMax || 0])
+    return d3.scaleLinear()
+      .domain([0, yMax ? yMax + 1000 : 0])
       .range([this.height, 0]);
-    
-    const customColors = ['#ffc000', '#0f9ed5', '#db5757', '#4ea72e', '#747474', '#d86ecc'];
-    const color = d3.scaleOrdinal(customColors).domain(categories);
+  }
 
-    const stackedData = d3.stack().keys(categories)(data);
-
-    console.log(stackedData[0]);
-
-    const areaGenerator = d3.area<any>()
-      .x(d => x(this.parseYearMonthToDate(d.data.month)))
-      .y0(d => y(d[0]))
-      .y1(d => y(d[1]));
-
-    this.svg.selectAll('.layer')
-      .data(stackedData)
-      .enter()
-      .append('path')
-      .attr('class', 'layer')
-      .attr('d', areaGenerator)
-      .style('fill', (d: { key: string; }) => color(d.key));
-
-
+  private drawAxes(x: d3.ScaleTime<number, number>, y: d3.ScaleLinear<number, number>) {
     this.svg.append('g')
       .attr('class', 'axis axis--x')
       .attr('transform', `translate(0,${this.height})`)
@@ -112,13 +104,31 @@ export class StackedAreaChartComponent implements OnInit {
     this.svg.append('g')
       .attr('class', 'axis axis--y')
       .call(d3.axisLeft(y));
+  }
 
+  private drawStackedAreas(data: any[], categories: string[], x: d3.ScaleTime<number, number>, y: d3.ScaleLinear<number, number>) {
+    const color = this.createColorScale(categories);
+    const stackedData = d3.stack().keys(categories)(data);
+    const areaGenerator = d3.area<any>()
+      .x(d => x(this.parseYearMonthToDate(d.data.month)))
+      .y0(d => y(d[0]))
+      .y1(d => y(d[1]));
 
+    this.svg.selectAll('.layer')
+      .data(stackedData)
+      .enter().append('path')
+      .attr('class', 'layer')
+      .attr('d', areaGenerator)
+      .style('fill', (d: { key: string; }) => color(d.key));
+  }
+
+  private drawLegend(categories: string[]) {
+    const color = this.createColorScale(categories);
     const legendSpacing = 4;
-    const legendRectSize = 12; 
-    const legendXOffset = this.width - 850; 
+    const legendRectSize = 12;
+    const legendXOffset = this.width - 850;
     const legendYOffset = 0;
-    
+
     const legend = this.svg.selectAll('.legend')
       .data(color.domain())
       .enter()
@@ -138,11 +148,13 @@ export class StackedAreaChartComponent implements OnInit {
       .style('fill', color)
       .style('stroke', color);
 
-    legend.append('text')
+      legend.append('text')
       .attr('x', legendRectSize + legendSpacing)
       .attr('y', legendRectSize - legendSpacing)
       .text(function(d: any) { return d; });
+  }
 
+  private annotateSpecialDates(x: d3.ScaleTime<number, number>) {
     const specialDate1 = new Date('2020-03-15');
       this.svg.append('line')
         .style('stroke', 'white') 
@@ -187,6 +199,15 @@ export class StackedAreaChartComponent implements OnInit {
       .attr('y', 100)
       .style('fill', 'black')
       .text('contre la COVID');
-      
+  }
+
+  private createColorScale(categories: string[]) {
+    const customColors = ['#ffc000', '#0f9ed5', '#db5757', '#4ea72e', '#747474', '#d86ecc'];
+    return d3.scaleOrdinal(customColors).domain(categories);
+  }
+
+  private parseYearMonthToDate(monthString: string): Date {
+    const [year, month] = monthString.split('-').map(Number);
+    return new Date(year, month - 1);
   }
 }
