081a0df2d58a25e7b7c63328187a1080f51d1794
RiadMefti <riadmefti@hotmail.com>
2024-04-21 16:34:00 -0400

    adding small navbvar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
-import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { RadialBarChartComponent } from './components/radial-bar-chart/radial-bar-chart.component';
 import { StackedAreaChartComponent } from './components/stacked-area-chart/stacked-area-chart.component';
+import { ChoroplethChartComponent } from './components/choropleth-chart/choropleth-chart.component';
+import { TreeMapChartComponent } from './components/tree-map-chart/tree-map-chart.component';
+import { ScatterPlotChartComponent } from './components/scatter-plot-chart/scatter-plot-chart.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', title: 'Home Page', component: HomePageComponent},
-    { path: 'home', title: 'Home Page', component: HomePageComponent},
-    { path : 'radial', title: 'Radial', component: RadialBarChartComponent},
-    { path: 'stacked-area', component: StackedAreaChartComponent }
+    { path: '', component: HomePageComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: 'radial-bar-chart', component: RadialBarChartComponent },
+    { path: 'stacked-area-chart', component: StackedAreaChartComponent },
+    { path: 'choropleth-chart', component: ChoroplethChartComponent },
+    { path: 'tree-map-chart', component: TreeMapChartComponent },
+    { path: 'scatter-plot-chart', component: ScatterPlotChartComponent }
 ];
