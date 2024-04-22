44268dc3fd65f5c2d9d9aa2606e8d57297e7c44a
amine <amine.bourdache@polymtl.ca>
2024-04-22 17:42:06 -0400

    auto scroll and always keep navbar on top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChoroplethChartComponent } from "./components/choropleth-chart/choropleth-chart.component";
+import { StackedAreaChartComponent } from "./components/stacked-area-chart/stacked-area-chart.component";
+import { RadialBarChartComponent } from "./components/radial-bar-chart/radial-bar-chart.component";
+import { TreeMapChartComponent } from "./components/tree-map-chart/tree-map-chart.component";
+import { ScatterPlotChartComponent } from "./components/scatter-plot-chart/scatter-plot-chart.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, ChoroplethChartComponent, StackedAreaChartComponent, RadialBarChartComponent, TreeMapChartComponent, ScatterPlotChartComponent]
 })
 export class AppComponent {
   title = 'montreal-crimes-analysis';
+
+  public scroll($element: any): void{
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
 }
